712ecabbd0c0320a4f814107b81334367b3aaf5d
anwer11919 <anwerahmed19@gmail.com>
2026-09-01 00:56:56 +0300

    fix: simplify admin to bare minimum to resolve 500 error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next"
+﻿import type { Metadata } from "next"
 import "./globals.css"
 import Providers from "@/components/Providers"
 import Navbar from "@/components/Navbar"
