1864ebe68191f19708788939bfdcf94cfa90d13c
anwer1919 <98690064+anwer1919@users.noreply.github.com>
2026-09-14 04:15:59 +0300

    Rename NooryiChatbot to C in ChatbotLoader

--- src/components/ChatbotLoader.tsx
+++ src/components/ChatbotLoader.tsx
@@ -1,4 +1,4 @@
 "use client";
 import dynamic from "next/dynamic";
-const NooryiChatbot = dynamic(() => import("@/components/NooryiChatbot"), { ssr: false, loading: () => null });
-export default function ChatbotLoader() { return <NooryiChatbot />; }
+const C = dynamic(() => import("@/components/NooryiChatbot"), { ssr: false, loading: () => null });
+export default function ChatbotLoader() { return <C />; }
